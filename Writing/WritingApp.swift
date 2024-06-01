52df29a6c4be78b9f5d2fc074e4aa3632b2c2c3b
bdreyerr <bendreyer21@gmail.com>
2024-06-01 15:00:00 -0700

    Initial Commit

---
+++ Writing/WritingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WritingApp.swift
+//  Writing
+//
+//  Created by Ben Dreyer on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WritingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
