1773ef96e897c4977a74a0f8fbd59ba46336e386
bdreyerr <bendreyer21@gmail.com>
2024-06-24 14:05:03 -0700

    - edit short - fix signed out profile page ui - only signed in users can write shorts

---
+++ Writing/Views/Profile/ProfileMainView-SignedIn/ProfileEditShortView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileEditShortView.swift
+//  Writing
+//
+//  Created by Ben Dreyer on 6/24/24.
+//
+
+import SwiftUI
+
+struct ProfileEditShortView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileEditShortView()
+}
